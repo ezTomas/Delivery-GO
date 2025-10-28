78e85bbb899083bb15312e0b0b49482b776de549
Enzo Tomas <195832517+ezTomas@users.noreply.github.com>
2025-10-28 13:52:36 -0300

    Implementacion de asets

--- nuevo-proyecto-de-juego/Scrips/mapa.gd
+++ nuevo-proyecto-de-juego/Scrips/mapa.gd
@@ -3,7 +3,12 @@ extends Node2D
 @onready var playerPoints: Label = $CanvasLayer/Panel/PlayerPoints
 @onready var rivalPoints: Label = $CanvasLayer/Panel2/RivalPoints
 
+
+
 var house1 = preload("res://Scene/Casa.tscn")
+var house2 = preload("res://Scene/Casa2.tscn")
+var house3 = preload("res://Scene/Casa3.tscn")
+
 var obstaculo = preload("res://Scene/Obstaculo.tscn")
 
 func _ready() -> void:
@@ -14,25 +19,58 @@ func _physics_process(delta: float) -> void:
 	rivalPoints.text = str(GlobalPoints.rivalPoints)
 
 func _on_spawn_house_timeout() -> void:
-	var randomHouse = round(randf_range(1,1))
 	
+	var randomHouse = round(randf_range(1,3))
 	if randomHouse == 1:
 		var houseSpawn = house1.instantiate()
 		self.call_deferred("add_child", houseSpawn)
 		houseSpawn.position.y = -60
-		houseSpawn.position.x = round(randf_range(200,200))
+		houseSpawn.position.x = round(randf_range(1070,1150))
 	
-	if randomHouse == 1:
-		var houseSpawn = house1.instantiate()
+	if randomHouse == 2:
+		var houseSpawn = house2.instantiate()
 		self.call_deferred("add_child", houseSpawn)
 		houseSpawn.position.y = -60
-		houseSpawn.position.x = round(randf_range(1000,1000))
+		houseSpawn.position.x = round(randf_range(1120,1200))
 
+	if randomHouse == 3:
+		var houseSpawn = house3.instantiate()
+		self.call_deferred("add_child", houseSpawn)
+		houseSpawn.position.y = -60
+		houseSpawn.position.x = round(randf_range(1070,1150))
+
+func _on_spawn_house_enemy_timeout() -> void:
+	var randomHouse = round(randf_range(1,3))
+	if randomHouse == 1:
+		var houseSpawn = house1.instantiate()
+		self.call_deferred("add_child", houseSpawn)
+		houseSpawn.position.y = -60
+		houseSpawn.position.x = round(randf_range(100,200))
+	
+	if randomHouse == 2:
+		var houseSpawn = house2.instantiate()
+		self.call_deferred("add_child", houseSpawn)
+		houseSpawn.position.y = -60
+		houseSpawn.position.x = round(randf_range(50,150))
+
+	if randomHouse == 3:
+		var houseSpawn = house3.instantiate()
+		self.call_deferred("add_child", houseSpawn)
+		houseSpawn.position.y = -60
+		houseSpawn.position.x = round(randf_range(100,200))
 
 func _on_spawn_obstaculo_timeout() -> void:
 	var randomObstaculo = round(randf_range(1,4))
-	if randomObstaculo <= 4:
+	if randomObstaculo <= 2:
 		var obstaculoSpawn = obstaculo.instantiate()
 		self.call_deferred("add_child", obstaculoSpawn)
 		obstaculoSpawn.position.y = -60
 		obstaculoSpawn.position.x = round(randf_range(700,900))
+
+func _on_spawnobstaculos_enemy_timeout() -> void:
+	var randomObstaculo = round(randf_range(1,4))
+	if randomObstaculo <= 2:
+		var obstaculoSpawn = obstaculo.instantiate()
+		self.call_deferred("add_child", obstaculoSpawn)
+		obstaculoSpawn.position.y = -60
+		obstaculoSpawn.position.x = round(randf_range(300,600))
--- nuevo-proyecto-de-juego/Scrips/rival.gd
+++ nuevo-proyecto-de-juego/Scrips/rival.gd
@@ -1,12 +1,19 @@
 extends CharacterBody2D
 
 var bullet = preload("res://Scene/BulletRival.tscn")
+@onready var stunTimer: Timer = $StunTimer
+var stun: bool
+
+@export var limite_right: int = 370
+@export var limite_left: int = 500
+
 
 func _ready() -> void:
 	randomize()
+	moverse()
 
 func _on_area_deteccion_area_entered(area: Area2D) -> void:
-	if area.is_in_group("Casa"):
+	if area.is_in_group("Casa") && stun == false:
 		var disparo = round(randf_range(1,5))
 		if disparo <= 2:
 			var offset_y = 230.0
@@ -14,9 +21,32 @@ func _on_area_deteccion_area_entered(area: Area2D) -> void:
 			var direction = (target_pos - global_position).normalized()
 			shoot(direction)
 
+func _on_area_rival_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Obstaculo"):
+		print("Stuneado")
+		stun = true
+		stunTimer.start()
+
+func _on_stun_timer_timeout() -> void:
+	stun = false
+
 func shoot(direction: Vector2): #Recibe hacia donde debe rotar
 	var newBullet = bullet.instantiate()
 	newBullet.global_position = $SpawnPointRival.global_position
 	newBullet.rotation = direction.angle() # Se usa para calcular el angulo y rotar la bala 
 	newBullet.direction = direction
 	get_parent().add_child(newBullet)
+
+func moverse():
+	while true:
+		await get_tree().create_timer(2).timeout
+		var moverse = randf_range(1,5)
+		var destino = position
+		
+		if moverse >= 4 && position.x < limite_left:
+			destino.x += 70
+		elif moverse <= 3 && position.x > limite_right:
+			destino.x -= 70
+		
+		var tween = create_tween()
+		tween.tween_property(self, "position", destino, 0.5)
