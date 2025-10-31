d0f4738145ede135da90e617634b9d3187295a49
Enzo Tomas <tomasenzo03@gmail.com>
2025-10-31 17:09:02 -0300

    Casas

--- nuevo-proyecto-de-juego/Scrips/mapa.gd
+++ nuevo-proyecto-de-juego/Scrips/mapa.gd
@@ -22,42 +22,25 @@ func _on_spawn_house_timeout() -> void:
 	
 	var randomHouse = round(randf_range(1,3))
 	if randomHouse == 1:
-		var houseSpawn = house1.instantiate()
-		self.call_deferred("add_child", houseSpawn)
-		houseSpawn.position.y = -60
-		houseSpawn.position.x = round(randf_range(1070,1150))
+		spawnCasa(house1)
 	
 	if randomHouse == 2:
-		var houseSpawn = house2.instantiate()
-		self.call_deferred("add_child", houseSpawn)
-		houseSpawn.position.y = -60
-		houseSpawn.position.x = round(randf_range(1120,1200))
+		spawnCasa(house2)
 
 	if randomHouse == 3:
-		var houseSpawn = house3.instantiate()
-		self.call_deferred("add_child", houseSpawn)
-		houseSpawn.position.y = -60
-		houseSpawn.position.x = round(randf_range(1070,1150))
+		spawnCasa(house3)
 
 func _on_spawn_house_enemy_timeout() -> void:
+	
 	var randomHouse = round(randf_range(1,3))
 	if randomHouse == 1:
-		var houseSpawn = house1.instantiate()
-		self.call_deferred("add_child", houseSpawn)
-		houseSpawn.position.y = -60
-		houseSpawn.position.x = round(randf_range(100,200))
+		spawnCasaEnemy(house1)
 	
 	if randomHouse == 2:
-		var houseSpawn = house2.instantiate()
-		self.call_deferred("add_child", houseSpawn)
-		houseSpawn.position.y = -60
-		houseSpawn.position.x = round(randf_range(50,150))
+		spawnCasaEnemy(house2)
 
 	if randomHouse == 3:
-		var houseSpawn = house3.instantiate()
-		self.call_deferred("add_child", houseSpawn)
-		houseSpawn.position.y = -60
-		houseSpawn.position.x = round(randf_range(100,200))
+		spawnCasaEnemy(house3)
 
 func _on_spawn_obstaculo_timeout() -> void:
 	var randomObstaculo = round(randf_range(1,4))
@@ -74,3 +57,15 @@ func _on_spawnobstaculos_enemy_timeout() -> void:
 		self.call_deferred("add_child", obstaculoSpawn)
 		obstaculoSpawn.position.y = -60
 		obstaculoSpawn.position.x = round(randf_range(300,600))
+
+func spawnCasa(house):
+		var houseSpawn = house.instantiate()
+		self.call_deferred("add_child", houseSpawn)
+		houseSpawn.position.y = -60
+		houseSpawn.position.x = round(randf_range(1090,1200))
+
+func spawnCasaEnemy(house):
+		var houseSpawn = house.instantiate()
+		self.call_deferred("add_child", houseSpawn)
+		houseSpawn.position.y = -60
+		houseSpawn.position.x = round(randf_range(100,200))
