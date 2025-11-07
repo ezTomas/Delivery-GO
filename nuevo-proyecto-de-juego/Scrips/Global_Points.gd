14815c7ea30667250724d101e2a39636d3807673
Enzo Tomas <195832517+ezTomas@users.noreply.github.com>
2025-11-07 19:20:05 -0300

    .

--- nuevo-proyecto-de-juego/Scrips/Global_Points.gd
+++ nuevo-proyecto-de-juego/Scrips/Global_Points.gd
@@ -13,15 +13,18 @@ var nivel: int = 0
 func _process(delta: float) -> void:
 	if nivel == 0 and points_nivel1 >= 10:
 		nivel += 1
+		get_tree().change_scene_to_file("res://Scene/Casas/control.tscn")
 	elif rivalPoints >= 10:
 		get_tree().change_scene_to_file("res://Scene/Casas/Menu.tscn")
 
 	if nivel == 1 and points_nivel2 >= 15:
 		nivel += 1
+		get_tree().change_scene_to_file("res://Scene/Casas/control.tscn")
 	elif rivalPoints >= 15:
 		get_tree().change_scene_to_file("res://Scene/Casas/Menu.tscn")
 	
 	if nivel == 2 and points_nivel3 >= 18:
 		nivel += 1
+		get_tree().change_scene_to_file("res://Scene/Casas/control.tscn")
 	elif rivalPoints >= 18:
 		get_tree().change_scene_to_file("res://Scene/Casas/Menu.tscn")
