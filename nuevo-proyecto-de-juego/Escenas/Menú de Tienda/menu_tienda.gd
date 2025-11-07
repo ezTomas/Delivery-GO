a51a51c539a23ada216b725b5c50caa6da31c329
Enzo Tomas <195832517+ezTomas@users.noreply.github.com>
2025-11-07 12:23:12 -0300

    Ajustes de interfaces (1)

--- nuevo-proyecto-de-juego/Escenas/Menú de Tienda/menu_tienda.gd
+++ nuevo-proyecto-de-juego/Escenas/Menú de Tienda/menu_tienda.gd
@@ -1,6 +1,20 @@
 extends Control
 
-
-
-func _on_button_button_down() -> void:
+func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Escenas/control.tscn")
+
+
+func _on_obtener_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_mejorar_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_ver_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_comprar_pressed() -> void:
+	pass # Replace with function body.
