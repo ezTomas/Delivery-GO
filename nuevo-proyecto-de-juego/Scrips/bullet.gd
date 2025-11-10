c05a1df311be1ca429a23f4eab7a1772df7c639f
Enzo Tomas <tomasenzo03@gmail.com>
2025-11-10 15:37:37 -0300

    Correciones

--- nuevo-proyecto-de-juego/Scrips/bullet.gd
+++ nuevo-proyecto-de-juego/Scrips/bullet.gd
@@ -11,6 +11,14 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func _on_area_bala_player_area_entered(area: Area2D) -> void:
+	
 	if area.is_in_group("Casa"):
-		GlobalPoints.points += 1
-		queue_free()
+		if GlobalPoints.nivel >= 0:
+			GlobalPoints.points_nivel1 += 1
+			queue_free()
+		if GlobalPoints.nivel >= 1:
+			GlobalPoints.points_nivel2 += 1
+			queue_free()
+		if GlobalPoints.nivel >= 2:
+			GlobalPoints.points_nivel3 += 1
+			queue_free()
